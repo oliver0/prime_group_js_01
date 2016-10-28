c7b9e4819dc796c8e7b581474a320b917e2894a8
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-10-28 10:34:22 -0500

    Changed initial arrays to objects

--- group_01.js
+++ group_01.js
@@ -1,19 +1,25 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
-var robert = ["Robert", "26835", "66000", 1];
-var mayella = ["Mayella", "89068", "35000", 2];
+var atticus = new Person("Atticus","2405", "47000", 3); //["Atticus", "2405", "47000", 3];
+var jem = new Person("Jem", "62347", "63500", 4);//("Jem", "62347", "63500", 4) //["Jem", "62347", "63500", 4];
+var boo = new Person("Boo", "11435", "54000", 3);//["Boo", "11435", "54000", 3];
+var scout = new Person("Scout", "6243", "74750", 5);//["Scout", "6243", "74750", 5];
+var robert = new Person("Robert", "26835", "66000", 1);//["Robert", "26835", "66000", 1];
+var mayella = new Person("Mayella", "89068", "35000", 2);//["Mayella", "89068", "35000", 2];
+
+function Person(name, employeeNumber, annualSalary, reviewRating){
+  this.name = name;
+  this.employeeNumber = employeeNumber;
+  this.annualSalary = annualSalary;
+  this.reviewRating = reviewRating;
+}
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
 
-// ["employees name", "bonus percentage", "base annual + bonus", "total bonus to nearest dollar"]
-
-function bonusCalculation(arrEmployees){
-  var employeeBonus = [];
+function bonusCalculation(employee){
+  var employeeBonus = {};
   var bonus = 0;
-  employeeBonus[0] = arrEmployees[0];
-switch(arrEmployees[3]) {
+  var name = employee.name;
+
+switch(employee.reviewRating) {
   case 1:
     bonus = 0;
     break;
@@ -31,10 +37,10 @@ switch(arrEmployees[3]) {
     break;
   default: return false;
   }
-  if (arrEmployees[1].length == 4) {
+  if (employee.employeeNumber.length == 4) {
     bonus += .05;
   }
-  if (arrEmployees[2] > 65000) {
+  if (employee.annualSalary> 65000) {
     bonus -= .01;
   }
   if (bonus > .13) {
@@ -43,23 +49,36 @@ switch(arrEmployees[3]) {
   if (bonus < 0) {
     bonus = 0;
   }
-  var bonusPercent = bonus * 100;
-  employeeBonus[1] = bonusPercent;
-  employeeBonus[2] = arrEmployees[2] * (1 + bonus);
-  employeeBonus[3] = Math.round(arrEmployees[2] * bonus);
+
+  employeeBonus.name = employee.name;
+  employeeBonus.bonusPercent = bonus * 100;
+  employeeBonus.annualSalary = employee.annualSalary * (1 + bonus);
+  employeeBonus.roundedBonus = Math.round(employee.annualSalary * bonus);
+
   return employeeBonus;
   }
 
+  var employeeBonusArray = [];
+  for(i=0; i < employees.length; i++){
+    employeeBonusArray.push(bonusCalculation(employees[i]));
+  }
+  console.log(employeeBonusArray);
 
+
+/*
+Below is previous code used to get the code on to HTML
 
 var statement = "";
 var newArray = [];
 for (var i = 0; i < employees.length; i++) {
-  var statementArray = bonusCalculation(employees[i]);
-  statement = statementArray[0] + "'s" + " bonus % is " + statementArray[1] + "%. Making their annual salary " + statementArray[2] + " including the bonus of "+ statementArray[3] + ".\n";
-  newArray.push(statement);
-}
+  //var statementArray = bonusCalculation(employees[i]);
 
+  //statement = statementArray[0] + "'s" + " bonus % is " + statementArray[1] + "%. Making their annual salary " + statementArray[2] + " including the bonus of "+ statementArray[3] + ".\n";
+
+  newArray.push(bonusCalculation(employees[i]));
+}
+//console.log(newArray);
+/*
 function makeUL() {
     var list = document.createElement('ul');
 
@@ -73,3 +92,4 @@ function makeUL() {
 }
 
 document.getElementById('container').appendChild(makeUL(newArray[0]));
+*/
